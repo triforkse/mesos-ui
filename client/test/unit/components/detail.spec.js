358927bb8f25dbc819c04a99cb909758afb54c60
Max Malm <benjick@dumfan.net>
2016-01-12 15:53:07 +0100

    Unit test for Detail component

---
+++ client/test/unit/components/detail.spec.js
@@ -0,0 +1,39 @@
+/* global it expect describe beforeEach */
+
+import React from 'react';
+import sd from 'skin-deep';
+const $ = React.createElement;
+
+import Detail from '../../../src/components/detail';
+
+describe('Detail Component', () => {
+  let tree;
+  let vdom;
+  const props = {
+    title: 'Hello',
+    close: () => {
+      return true;
+    },
+  };
+
+  beforeEach(() => {
+    tree = sd.shallowRender(
+      $(Detail, props,
+        $('div', {className: 'someChild'}, 'bar')
+      )
+    );
+    vdom = tree.getRenderOutput();
+  });
+
+  it('should render with a correct class name', () => {
+    expect(vdom.props.className).to.eql('detail');
+  });
+
+  it('should have children', () => {
+    expect(tree.textIn('.someChild')).to.eql('bar');
+  });
+
+  it('should display children text', () => {
+    expect(tree.textIn('.detail-title')).to.eql('Hello (close)');
+  });
+});
