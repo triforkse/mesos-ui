d2300e1a4f5ff0e3eadf3dc3d6371057b4361855
Max Malm <benjick@dumfan.net>
2016-01-13 21:16:42 +0100

    Change code to fit new redux-devtools api

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react'; // eslint-disable-line
 import configureStore from './store/configureStore';
 import {ReduxRouter} from 'redux-router';
 
@@ -16,9 +15,6 @@ render(
     <Provider store={store}>
       <ReduxRouter />
     </Provider>
-    <DebugPanel top right bottom>
-      {/* <DevTools store={store} monitor={LogMonitor} /> */}
-    </DebugPanel>
   </div>,
   document.getElementById('app')
 );
